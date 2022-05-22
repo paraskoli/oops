30aaf8d9ba6d826e2892ab99942aa00b72b5c6b7
paraskoli <paraskoli43@gmail.com>
2022-05-22 12:17:59 +0530

    oops

--- OOPS.java
+++ OOPS.java
@@ -10,19 +10,37 @@ class Pen{
     System.out.println(this.color);        
     }
 }
+ class Student {
+     String name;
+     int age;
+     public void printinfo()
+     {
+         System.out.println(this.name);
+         System.out.println(this.age);
+    
 
+     }
+ }
 // main function
 public class OOPS {
-  public static void main(String[]args) {
-      Pen pen1 =new Pen(); // object
-      pen1.color="blue";
-      pen1.type="gel";
+//   public static void main(String[]args) {
+//       Pen pen1 =new Pen(); // object
+//       pen1.color="blue";
+//       pen1.type="gel";
 
-      Pen pen2=new Pen();
-      pen2.color ="black";
-      pen2.type="ballpoint";
+//       Pen pen2=new Pen();
+//       pen2.color ="black";
+//       pen2.type="ballpoint";
 
-      pen1.printcolor();
-      pen2.printcolor();
-    }  
+//       pen1.printcolor();
+//       pen2.printcolor();
+//     }  
+public static void main(String[] args) {
+    Student s1=new Student();
+    s1.name="paras";
+    s1.age=22;
+
+    s1.printinfo();
+}
+
 }
